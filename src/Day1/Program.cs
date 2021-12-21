11b8025bf508a04018dc3899523e5f092f91caa4
ideefiix <flindu@gmail.com>
2021-12-21 21:05:46 +0100

    Day1 finished

---
+++ src/Day1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+List<int> integers = new List<int>();
+int answer1 = 0;
+// Read input
+foreach (string line in System.IO.File.ReadLines("input.txt"))
+{
+    integers.Add(int.Parse(line));
+}
+
+foreach (int num in integers)
+{
+    foreach (int num2 in integers)
+    {
+        foreach (int num3 in integers)
+        {
+            if (num + num2 + num3 == 2020)
+            {
+                answer1 = num * num2 * num3;
+            }
+        }
+    }
+}
+
+Console.WriteLine("Answer task 2: " + answer1);
